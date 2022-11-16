f070873a89e6545ec7b3adbe703a8a7ac0006133
hennadii-demchenko <impskievua@gmail.com>
2022-11-16 23:23:43 +0200

    Add base classes
    Signed-off-by: hennadii-demchenko <impskievua@gmail.com>

---
+++ ms/main.py
@@ -0,0 +1,28 @@
+import pygame
+
+
+class Grid:
+    """board generation"""
+
+
+class Fog:
+    """board uncovering"""
+
+
+class Game:
+    """
+    facade for
+    - settings
+    - stats
+    - start/restart game
+    - tbd
+
+    """
+
+
+def main() -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
