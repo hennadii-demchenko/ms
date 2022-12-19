825391fb58107602cec4a85c2a4ca98bb3ccae2d
hennadii-demchenko <impskievua@gmail.com>
2022-12-19 22:57:49 +0200

    Add more draw figures
    Signed-off-by: hennadii-demchenko <impskievua@gmail.com>

--- ms/draw.py
+++ ms/draw.py
@@ -1,12 +1,13 @@
 import math
 
 import pygame.draw
-from pygame import Rect, Surface, Color
+from pygame import Rect, Color
 
 BG_COLOR = Color(0xC0, 0xC0, 0xC0)
 SHADOW_COLOR = Color(0x80, 0x80, 0x80)
 HIGHLIGHT_COLOR = Color(0xFF, 0xFF, 0xFF)
 NUM_COLORS = {
+    0: BG_COLOR,
     1: Color(0x0, 0x0, 0xFF),
     2: Color(0x0, 0x80, 0x0),
     3: Color(0xFF, 0x0, 0x0),
@@ -18,14 +19,62 @@ NUM_COLORS = {
 }
 
 
-def draw_mine(surface: Surface, rect: Rect, exploded: bool = False) -> None:
-    if exploded:
-        pygame.draw.rect(surface, "red", rect)
+def draw_flag(rect: Rect):
+    screen = pygame.display.get_surface()
+    offset = max(rect.height, rect.width) // 4
+    pygame.draw.polygon(
+        screen,
+        "black",
+        [
+            (rect.left + offset, rect.bottom - offset // 2),
+            (rect.centerx, rect.centery + offset),
+            (rect.right - offset, rect.bottom - offset // 2),
+        ],
+    )
+    pygame.draw.line(
+        screen,
+        "black",
+        (rect.centerx, rect.centery + offset),
+        rect.center,
+        offset // 6
+    )
+    pygame.draw.line(
+        screen,
+        "red",
+        rect.center,
+        (rect.centerx, rect.top + offset // 1.5),
+        offset // 6
+    )
 
-    pygame.draw.rect(surface, SHADOW_COLOR, rect, width=1)
+    pygame.draw.polygon(
+        screen,
+        "red",
+        [
+            rect.center,
+            (rect.centerx, rect.top + offset // 1.5),
+            (rect.centerx - rect.width // 3, rect.top + offset * 1.5),
+
+        ],
+    )
+
+
+def draw_reset(rect: Rect):
+    pygame.draw.rect(pygame.display.get_surface(), BG_COLOR, rect)
+
+
+def draw_empty(rect: Rect, shadow=SHADOW_COLOR):
+    pygame.draw.rect(pygame.display.get_surface(), shadow, rect, width=1)
+
+
+def draw_mine(rect: Rect, exploded: bool = False) -> None:
+    screen = pygame.display.get_surface()
+    if exploded:
+        pygame.draw.rect(screen, "red", rect)
+
+    pygame.draw.rect(screen, SHADOW_COLOR, rect, width=1)
     radius = max(rect.height, rect.width) / 3.5
     rays_size = int(radius // 4)
-    circle = pygame.draw.circle(surface, "black", rect.center, radius)
+    circle = pygame.draw.circle(screen, "black", rect.center, radius)
     cx, cy = circle.center
 
     # gloss
@@ -33,42 +82,43 @@ def draw_mine(surface: Surface, rect: Rect, exploded: bool = False) -> None:
         circle.center[0] - radius / 6,
         circle.center[1] - radius / 6,
     )
-    pygame.draw.circle(surface, SHADOW_COLOR, reflect_center, radius // 2)
-    pygame.draw.circle(surface, BG_COLOR, reflect_center, radius // 3)
-    pygame.draw.circle(surface, HIGHLIGHT_COLOR, reflect_center, radius // 8)
+
+    pygame.draw.circle(screen, SHADOW_COLOR, reflect_center, radius // 2)
+    pygame.draw.circle(screen, BG_COLOR, reflect_center, radius // 3)
+    pygame.draw.circle(screen, HIGHLIGHT_COLOR, reflect_center, radius // 8)
 
     # radial rays
     pygame.draw.line(  # top
-        surface,
+        screen,
         "black",
         circle.midtop,
-        (circle.midtop[0], circle.midtop[1] - rays_size),
+        (circle.centerx, circle.top - rays_size),
         rays_size,
     )
     pygame.draw.line(  # bottom
-        surface,
+        screen,
         "black",
         circle.midbottom,
-        (circle.midbottom[0], circle.midbottom[1] + rays_size),
+        (circle.centerx, circle.bottom + rays_size),
         rays_size,
     )
     pygame.draw.line(  # right
-        surface,
+        screen,
         "black",
         circle.midright,
-        (circle.midright[0] + rays_size, circle.midright[1]),
+        (circle.right + rays_size, circle.centery),
         rays_size,
     )
     pygame.draw.line(  # left
-        surface,
+        screen,
         "black",
         circle.midleft,
-        (circle.midleft[0] - rays_size, circle.midleft[1]),
+        (circle.left - rays_size, circle.centery),
         rays_size,
     )
 
     pygame.draw.rect(  # top right (45)
-        surface,
+        screen,
         "black",
         (
             cx - rays_size // 2 + radius * math.cos(-(math.pi / 4)),
@@ -79,7 +129,7 @@ def draw_mine(surface: Surface, rect: Rect, exploded: bool = False) -> None:
     )
 
     pygame.draw.rect(  # top left (315)
-        surface,
+        screen,
         "black",
         (
             cx - rays_size // 2 + radius * math.cos(-(3 * math.pi / 4)),
@@ -90,7 +140,7 @@ def draw_mine(surface: Surface, rect: Rect, exploded: bool = False) -> None:
     )
 
     pygame.draw.rect(  # bottom right (135)
-        surface,
+        screen,
         "black",
         (
             cx - rays_size // 2 + radius * math.cos(math.pi / 4),
@@ -101,7 +151,7 @@ def draw_mine(surface: Surface, rect: Rect, exploded: bool = False) -> None:
     )
 
     pygame.draw.rect(  # bottom left (225)
-        surface,
+        screen,
         "black",
         (
             cx - rays_size // 2 + radius * math.cos(3 * math.pi / 4),
@@ -112,7 +162,8 @@ def draw_mine(surface: Surface, rect: Rect, exploded: bool = False) -> None:
     )
 
 
-def draw_border(surface: Surface, rect: Rect, inverted: bool = False) -> None:
+def draw_border(rect: Rect, inverted: bool = False) -> None:
+    screen = pygame.display.get_surface()
     thickness = max(rect.height, rect.width) // 12
     if inverted:
         highlight, shadow = SHADOW_COLOR, HIGHLIGHT_COLOR
@@ -120,29 +171,30 @@ def draw_border(surface: Surface, rect: Rect, inverted: bool = False) -> None:
         highlight, shadow = HIGHLIGHT_COLOR, SHADOW_COLOR
 
     pygame.draw.polygon(
-        surface,
+        screen,
         highlight,
         [
             rect.bottomleft,
             rect.topleft,
             rect.topright,
-            (rect.topright[0] - thickness, rect.topright[1] + thickness),
-            (rect.topleft[0] + thickness, rect.topleft[1] + thickness),
-            (rect.bottomleft[0] + thickness, rect.bottomleft[1] - thickness),
+            (rect.right - thickness, rect.top + thickness),
+            (rect.left + thickness, rect.top + thickness),
+            (rect.left + thickness, rect.bottom - thickness),
             rect.bottomleft,
         ],
     )
 
     pygame.draw.polygon(
-        surface,
+        screen,
         shadow,
         [
             rect.bottomleft,
             rect.bottomright,
             rect.topright,
-            (rect.topright[0] - thickness, rect.topright[1] + thickness),
-            (rect.bottomright[0] - thickness, rect.bottomright[1] - thickness),
-            (rect.bottomleft[0] + thickness, rect.bottomleft[1] - thickness),
+            (rect.right - thickness, rect.top + thickness),
+            (rect.right - thickness, rect.bottom - thickness),
+            (rect.left + thickness, rect.bottom - thickness),
             rect.bottomleft,
         ],
     )
+    draw_empty(rect, shadow)
